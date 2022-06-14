6aafad1e5fd7ee22d9d4588350eccd7313d2a7d9
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-13 20:36:03 -0700

    Add mouseover events to change the span text in the footer when hovered over, and add conditional statements to change the style applied to the page when the Dark/Light toggle is clicked

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+var sun = document.querySelector(".sun");
+var plantType = document.querySelector(".plant-type");
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var plantName = document.querySelector("h1");
+var body = document.querySelector("body");
 
+sun.addEventListener("mouseover", function () {
+  sun.innerText = "low to bright indirect light";
+});
+plantType.addEventListener("mouseover", function () {
+  plantType.innerText = "evergreen tropical vine";
+});
+
+toggle.addEventListener("click", function () {
+  if (body.classList.contains("light")) {
+    body.classList.remove("light");
+    ball.classList.add("move-right");
+    plantName.innerText = "Monstera deliciosa, aka split-leaf philodendron!";
+  } else {
+    body.classList.add("light");
+    ball.classList.remove("move-right");
+    plantName.innerText = "Monstera deliciosa — big, bold & beautiful!";
+  }
+});
